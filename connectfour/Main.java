f137794846e803b5a25854c0ca489e234c35c13a
Heeeevi <mbohwes0303@gmail.com>
2024-12-05 08:23:59 +0700

    nginx

---
+++ connectfour/Main.java
@@ -0,0 +1,9 @@
+package connectfour;
+
+public class Main {
+    public static void main(String[] args){
+        //TIP Press
+        System.out.println("Connect Four Game");
+        ConnectFour game = new ConnectFour();
+    }
+}
