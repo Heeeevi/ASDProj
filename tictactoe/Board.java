3b5c61491235cb5e594a89f1b15689f3b0a1d088
eric-vincentius <eric.vincentius.jaolis@gmail.com>
2024-12-19 08:30:46 +0700

    Does a bit editing

--- tictactoe/Board.java
+++ tictactoe/Board.java
@@ -81,7 +81,10 @@ public class Board {
                 && cells[4][0].content == player) {
             SoundEffect.YEAY.play();
             Object[] choice = {"Play Again", "Exit"};
-            int option = JOptionPane.showOptionDialog(null, player + "Wins", "Do you want to play again?", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, choice, choice[0]);
+            String winningPlayer = "";
+            if(player == Seed.CROSS) winningPlayer = "Maxwell";
+            else winningPlayer = "Oiia Cat";
+            int option = JOptionPane.showOptionDialog(null, winningPlayer + " Wins", "Do you want to play again?", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, choice, choice[0]);
             if(option == 0) newGame();
             else System.exit(0);
             return (player == Seed.CROSS) ? State.CROSS_WON : State.NOUGHT_WON;
