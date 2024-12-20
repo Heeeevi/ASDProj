f19177b37aa33b70bcf947cfc719acc56eec3c79
CowCatt <162095280+CowCatt@users.noreply.github.com>
2024-12-20 20:46:18 +0700

    Update State.java

--- connectfour/State.java
+++ connectfour/State.java
@@ -13,5 +13,5 @@ package connectfour;
  *  The enum State contains the various game states of the TTT game
  */
 public enum State {  // to save as "State.java"
-    PLAYING, DRAW, CROSS_WON, NOUGHT_WON
+    PLAYING, DRAW, RED_WON, BLUE_WON
 }
